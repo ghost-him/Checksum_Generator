dda76d17373e6f37698e8e5451ad3542467316b3
ghost_him <70733461+ghost-him@users.noreply.github.com>
2022-03-22 22:03:48 +0800

    Add files via upload

--- Checksum_Generator.go
+++ Checksum_Generator.go
@@ -9,12 +9,18 @@ import (
 	"flag"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"os"
 	"path/filepath"
 	"strings"
 	"time"
 )
 
+const (
+	MAX_FILE = 512
+)
+
+var dir_path string                              // 文件夹的路径
 var file_path string                             // 文件的路径
 var mode map[string]bool = make(map[string]bool) // 文件的模式
 var file_output_path string                      // 输出文件的路径
@@ -22,10 +28,13 @@ var now_time string                              // 现在的时间
 
 var is_file_path_true bool = false // 文件的路径是否正确
 var is_spawn_file bool = false     // 是否生成文件
+var is_dir_path bool = false       // 是否是一个文件夹
 
 var abs_path string  // 文件的绝对路径
 var file_name string // 文件的名字
 var mode_text string
+
+var program_text string
 
 var (
 	md5_code    string
@@ -38,13 +47,21 @@ func init() {
 	var slice = []string{} // 临时的切片
 	var modeT string       // 临时的变量
 	flag.StringVar(&file_path, "f", "", "文件的路径 绝对路径或者是相对路径")
-	flag.StringVar(&modeT, "mode", "md5", "加密的方式 md5 / sha1 / sha256 / sha512 / all 通过 , 来实现多种模式的组合")
+	flag.StringVar(&modeT, "m", "md5", "加密的方式 md5 / sha1 / sha256 / sha512 / all 通过 , 来实现多种模式的组合")
 	flag.StringVar(&file_output_path, "o", "", "结果的输出路径, 若不填则不生成文件")
+	flag.StringVar(&dir_path, "d", "", "文件夹的路径,会循环遍历所有的文件, 如果为空,则读取文件的路径")
 	flag.Parse() // 读取命令行
 
 	slice = strings.Split(modeT, ",")
-	for i := 0; i < len(slice); i++ {
-		mode[slice[i]] = true
+	for _, i := range slice {
+		mode[i] = true
+		mode_text += i + " "
+	}
+
+	if dir_path == "" { // 若为空,则为读取文件
+		is_dir_path = false
+	} else {
+		is_dir_path = true
 	}
 
 	// 获取时间
@@ -90,13 +107,29 @@ func file_sha512(file_path *os.File) string {
 	return hex.EncodeToString(sha512h.Sum(nil))
 }
 
+func init_text(file_path_ *os.File) {
+	if is_dir_path {
+		get_file_name(&dir_path)
+	} else {
+		get_file_name(&file_path)
+	}
+	program_text += "当前的时间: " + now_time + "\n"
+	program_text += "注意：如果字符串中包含有回车(\\r)换行符(\\n)等，因为不同的系统存在差异则会导致加密结果不同。所以最好将字符串以你需要的格式保存为文件后再选择文件加密！\n"
+	program_text += "文件路径: " + abs_path + "\n"
+	program_text += "当前的模式: " + mode_text + "\n"
+	if is_spawn_file {
+		file_path_.WriteString(program_text)
+	} else {
+		fmt.Println(program_text)
+	}
+}
+
 func answer_write(file_path_ *os.File) {
-	output_text := "当前的时间: " + now_time + "\n"
-	output_text += "注意：如果字符串中包含有回车(\\r)换行符(\\n)等，因为不同的系统存在差异则会导致加密结果不同。所以最好将字符串以你需要的格式保存为文件后再选择文件加密！\n"
-	output_text += "文件路径: " + abs_path + file_name + "\n"
-	output_text += "文件名字: " + file_name
-	output_text += "当前的模式: " + mode_text
-	output_text += "\n\n"
+	output_text := "\n文件名字: " + file_name
+	if d, err := os.Stat(abs_path + file_name); err == nil && d.IsDir() {
+		output_text += "(dir)"
+	}
+	output_text += "\n"
 
 	if md5_code != "" {
 		output_text += "md5(32位小写): " + md5_code + "\n"
@@ -119,56 +152,53 @@ func answer_write(file_path_ *os.File) {
 		file_path_.WriteString(output_text)
 	} else {
 		fmt.Println(output_text)
-		var temp int
-		fmt.Scan(&temp)
 	}
 }
 
 // 计算
 func compute(input_file_path, output_file_path *os.File) {
+	md5_code = ""
+	sha1_code = ""
+	sha256_code = ""
+	sha512_code = ""
 	if mode["all"] {
-		mode_text += "md5 sha1 sha256 sha512"
 		md5_code = file_md5(input_file_path)
 		sha1_code = file_sha1(input_file_path)
 		sha256_code = file_sha256(input_file_path)
 		sha512_code = file_sha512(input_file_path)
 	} else {
 		if mode["md5"] {
-			mode_text += "md5 "
 			md5_code = file_md5(input_file_path)
 		}
 		if mode["sha1"] {
-			mode_text += "sha1 "
 			sha1_code = file_sha1(input_file_path)
 		}
 		if mode["sha256"] {
-			mode_text += "sha256 "
 			sha256_code = file_sha256(input_file_path)
 		}
 		if mode["sha512"] {
-			mode_text += "sha512 "
 			sha512_code = file_sha512(input_file_path)
 		}
 	}
 	answer_write(output_file_path)
 }
 
-func get_file_name() {
+func get_file_name(file_path_ *string) {
 
 	// 获取当前文件的目录
-	if filepath.IsAbs(file_path) {
-		abs_path = filepath.Dir(file_path)
-		file_name = file_path[strings.Index(file_path, abs_path)+len(abs_path)+1:]
+	if filepath.IsAbs(*file_path_) {
+		abs_path = filepath.Dir(*file_path_)
+		file_name = (*file_path_)[strings.Index(*file_path_, abs_path)+len(abs_path)+1:]
 	} else {
-		file_name = file_path
-		abs_path, _ = filepath.Abs(file_path)
+		file_name = *file_path_
+		abs_path, _ = filepath.Abs(*file_path_)
 		abs_path = filepath.Dir(abs_path)
 	}
 	abs_path += "\\"
 }
 
-func check_file(file_path string) *os.File {
-	input_fileP, err := os.Open(file_path)
+func check_file(file_path_ *string) *os.File {
+	input_fileP, err := os.Open(*file_path_)
 	if err != nil {
 		is_file_path_true = false
 	} else {
@@ -177,29 +207,72 @@ func check_file(file_path string) *os.File {
 	return input_fileP
 }
 
+func single_file_mode(input_fileP, output_fileP *os.File) {
+	// 检测输入的文件
+	input_fileP = check_file(&file_path)
+	get_file_name(&file_path)
+	for !is_file_path_true {
+		file_path = ""
+		fmt.Println("检测不到文件,当前的文件路径是 " + abs_path + file_name + "\n请输入文件路径")
+		fmt.Scanln(&file_path)
+		input_fileP = check_file(&file_path)
+		get_file_name(&file_path)
+	}
+	compute(input_fileP, output_fileP)
+}
+
+func dir_mode(input_fileP, output_fileP *os.File) {
+	for _, err := os.Stat(dir_path); err != nil; {
+		fmt.Println("检测不到文件夹,检查是否拼写错误: " + dir_path + "\n请重新输入文件夹的路径: ")
+		fmt.Scanln(&dir_path)
+	}
+
+	var file_path_all []string = make([]string, 0)
+	add_file(&file_path_all, dir_path)
+
+	length := len(file_path_all)
+	for index, key := range file_path_all {
+		if length > 100 && index%(length/100) == 0 {
+			fmt.Printf("%d %% \n", index/(length/100))
+		}
+
+		input_fileP = check_file(&key)
+		get_file_name(&key)
+		compute(input_fileP, output_fileP)
+	}
+
+}
+
+func add_file(file_path_all *[]string, filepath string) {
+	file_col, err := ioutil.ReadDir(filepath)
+	if err == nil {
+		for _, file := range file_col {
+			if file.IsDir() {
+				add_file(file_path_all, filepath+"\\"+file.Name())
+			} else {
+				(*file_path_all) = append(*file_path_all, filepath+"\\"+file.Name())
+			}
+		}
+	}
+
+}
+
 func main() {
 	var output_fileP *os.File // 输出文件的指针
 	var input_fileP *os.File  // 输入文件的指针
 	defer output_fileP.Close()
 	defer input_fileP.Close()
 
-	// 检测输入的文件
-	input_fileP = check_file(file_path)
-	get_file_name()
-	for !is_file_path_true {
-		file_path = ""
-		fmt.Println("检测不到文件,当前的文件路径是 " + abs_path + file_name + "\n请输入文件路径")
-		fmt.Scanln(&file_path)
-		input_fileP = check_file(file_path)
-		get_file_name()
-	}
-
-	// 打开输出的文件
 	if is_spawn_file {
-		output_fileP, _ = os.OpenFile(file_output_path, os.O_CREATE|os.O_RDWR, 0666) // 创建输出文件
-		defer output_fileP.Close()                                                   // 关闭文件
+		output_fileP, _ = os.OpenFile(file_output_path, os.O_CREATE|os.O_RDWR|os.O_TRUNC, 0666) // 创建输出文件
 	}
 
-	compute(input_fileP, output_fileP)
+	init_text(output_fileP)
+
+	if !is_dir_path { // 读文件的模式
+		single_file_mode(input_fileP, output_fileP)
+	} else { // 读文件夹的模式
+		dir_mode(input_fileP, output_fileP)
+	}
 
 }
